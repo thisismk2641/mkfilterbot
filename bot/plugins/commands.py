93577d4b3692433166e8da3de66bf2e867e19927
thisismk2641 <85360630+thisismk2641@users.noreply.github.com>
2021-06-09 12:36:28 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'join our channel', url="https://t.me/new_ott_movies1"
                                 )
                         ]
                     ]
